6f344d887d5d63d258b890053b43c3889508a6df
reshithatalluri <rishithatalluri882@gmail.com>
2025-09-23 21:10:53 +0530

    Initial commit - Event Manager project

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import reportWebVitals from "./reportWebVitals";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
+
+// Optional performance logging
+reportWebVitals();
